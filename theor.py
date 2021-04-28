974b315bb9b1a0571a8bd074b82ce65dcbed3f1d
RFfront <69211010+RFfront@users.noreply.github.com>
2021-04-28 16:34:04 +0300

    laplas theorem

--- theor.py
+++ theor.py
@@ -1,6 +1,9 @@
 import decimal      #  импортируем модуль
 from decimal import Decimal
 from fractions import Fraction
+from math import e,pi,exp
+
+
 def mult(to,frome=0):
     t=1
     frome+=1
@@ -12,43 +15,55 @@ def c(m,n):
     return mult(n,n-m)/mult(m)
 
 def bern(n,p,k):
-
     decimal.getcontext().prec = 5    #  устанавливаем точность
     re=  Decimal(c(k,n))*   (Decimal(p)**k) *    ((1-Decimal(p))**(n-k) )
     return re
 
-#print(bern(8012322,0.3333333,1233313).quantize(Decimal('1.0000')))
+
 def gaussFoLaplas(x):
-    import math
-    from math import e,pi
-    #print(math.exp(0.1))
-    #decimal.getcontext().prec = 10
+
     x=Decimal(x)
 
     t=(1/(2*Decimal(pi)).sqrt())
-    figulina=((x**2)/2)
+    figulina=-((x**2)/2)
     ee=Decimal(e)**figulina
-    print(ee)
     ret=t * ee
-
     return ret
 
-print(gaussFoLaplas(3.99))
 def cdf(x):
+    x=Decimal(x)
+    f=1
+    if x<0:
+        f*=-1
+        x*=-1
     """
     Расчет функции Лапласа.
     """
-    import numpy as np
-    x = x / 1.414213562
-    a1 = 0.254829592
-    a2 = -0.284496736
-    a3 = 1.421413741
-    a4 = -1.453152027
-    a5 = 1.061405429
-    p = 0.3275911
 
-    t = 1 / (1 + p * x)
-    b = np.exp(-x * x)
+    x = x / Decimal(1.414213562)
+    a1 = Decimal(0.254829592)
+    a2 = Decimal(-0.284496736)
+    a3 = Decimal(1.421413741)
+    a4 = Decimal(-1.453152027)
+    a5 = Decimal(1.061405429)
+    p = Decimal(0.3275911)
+
+    t = Decimal(1 / (1 + p * x))
+    b = Decimal(exp(-x * x))
     y = (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * b
-    return (1 - y / 2)-0.5
-#print(cdf(2.5))
+    return ((1 - y / 2)-Decimal(0.5))*f
+
+def xFoLap(n,p,k):
+    q=1-p
+    return (k-n*p)/((n*p*q).sqrt())
+
+def laplas(n,p,k,k2=False):
+    q=1-p
+    decimal.getcontext().prec = 6
+    if not k2:
+        return (1/((n*p*q).sqrt())*gaussFoLaplas(xFoLap(n,p,k)))
+
+print(laplas(400,Decimal(0.5),225))
+print(bern(400,Decimal(0.5),225))
+
+print(cdf(2.5))
